820968976ccc4433c83c5981984a723279bf694b
ansbaby7 <ansbaby7@gmail.com>
2023-05-01 02:04:17 +0530

    Add TCP SYN scan

--- src/port_scanner/scan.py
+++ src/port_scanner/scan.py
@@ -1,6 +1,8 @@
 import socket
 import os
 import threading
+from scapy.layers.inet import IP, TCP
+from scapy.sendrecv import sr1
 
 def scan_ports(host_ip, start, end):
     for port in range(start, end  + 1):
@@ -11,7 +13,7 @@ def scan_ports(host_ip, start, end):
             print(f'Port {port} is open')
         sock.close()
 
-def tcp_connect_scan(host_ip, start_port, end_port, num_threads=30):
+def tcp_connect_scan(host_ip, start_port, end_port, num_threads=35):
     threads = []
     subrange_size = (end_port - start_port + 1) // num_threads
     if subrange_size < 1:
@@ -33,15 +35,36 @@ def tcp_connect_scan(host_ip, start_port, end_port, num_threads=30):
     for t in threads:
         t.join()
 
-def tcp_syn_scan(host_ip, start_port, end_port):
-    open_ports_count = 0
-    try:
-        for port in range(start_port, end_port + 1):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-            # 
-            sock.close()
-    except KeyboardInterrupt:
-        print("Scan terminated by user")
+def syn_scan_ports(host_ip, start, end):
+    for port in range(start, end + 1):
+        packet = IP(dst=host_ip)/TCP(dport=port, flags="S")
+        response = sr1(packet, timeout=0.5, verbose=0)
+        
+        if response:
+            if response[TCP].flags == "SA":
+                print(f'Port {port} is open')
+
+def tcp_syn_scan(host_ip, start_port, end_port, num_threads=35):
+    threads = []
+    subrange_size = (end_port - start_port + 1) // num_threads
+    if subrange_size < 1:
+        num_threads = 1
+    subranges = []
+
+    for i in range(num_threads):
+        start = start_port + i * subrange_size
+        end = start_port + (i + 1) * subrange_size - 1
+        if i == num_threads - 1:
+            end = end_port
+        subranges.append((start, end))
+    
+    for start, end in subranges:
+        t = threading.Thread(target=syn_scan_ports, args=(host_ip, start, end))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
 
 def ping_hosts(network_ip, start, end):
     for i in range(start, end + 1):
@@ -91,7 +114,20 @@ def scan(type):
         tcp_connect_scan(host_ip, int(start_port), int(end_port))
     
     elif type == "2":
-        pass
+        print("Specify the IPv4 address of the host to scan:", end=" ")
+        host = input()
+        host_ip = socket.gethostbyname(host)
+        print(host_ip)
+        print("Specify the port range to scan")  # need to handle unexpected inputs
+        print("Start port (if not specified, defaults to 1):", end=" ")
+        start_port = input()
+        if start_port == "" or start_port == "0":
+            start_port = 1
+        print("End_port (if not specified, defaults to 1024):", end=" ")
+        end_port = input()
+        if end_port == "":
+            end_port = 1024
+        tcp_syn_scan(host_ip, int(start_port), int(end_port))
 
     elif type == "3":
         print("Specify the network IPv4 address to scan:", end=" ")
